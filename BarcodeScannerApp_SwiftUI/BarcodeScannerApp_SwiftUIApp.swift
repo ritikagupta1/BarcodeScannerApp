d57a3bbdcfdb4fa7bdc572236958780dfb1fd921
Ritika Gupta <ritikagupta2020@gmail.com>
2025-01-06 13:48:20 +0530

    Initial Commit

---
+++ BarcodeScannerApp_SwiftUI/BarcodeScannerApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp_SwiftUIApp.swift
+//  BarcodeScannerApp_SwiftUI
+//
+//  Created by Ritika Gupta on 06/01/25.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
